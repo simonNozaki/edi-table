2d3ecf913be01d2b8ad08becf0400da94ebba5d0
snozaki <snozaki@netprotections.co.jp>
2023-04-28 21:14:03 +0900

    ヘッダセル用にコンポーネント作る

---
+++ src/components/EditableHeaderCell.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  textAlign?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  textAlign: 'center'
+})
+
+const classes = {
+  [props.textAlign]: true
+};
+</script>
+
+<template>
+  <div class="inline-block cell" :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.cell {
+  @apply w-full bg-teal-200 text-gray-800 p-1 font-semibold;
+}
+
+.center {
+  @apply text-center;
+}
+
+.left {
+  @apply text-left;
+}
+</style>
