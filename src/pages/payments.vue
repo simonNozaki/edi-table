d50d3b601d47fd4efe884034aac170e01999d5c5
snozaki <snozaki@netprotections.co.jp>
2023-05-02 21:05:24 +0900

    全体にタイトルをつける

--- src/pages/payments.vue
+++ src/pages/payments.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import Card from '@/components/atoms/Card.vue';
 import PaymentsList from '@/components/PaymentsList.vue';
 import { usePayments } from '@/composables/usePayments';
 const { payments } = usePayments()
 </script>
 
 <template>
-  <PaymentsList :values="payments" />
+  <Card>
+    <h1 class="title">支払明細</h1>
+    <PaymentsList :values="payments" />
+  </Card>
 </template>
+
+<style lang="css" scoped>
+.title {
+  @apply my-4 text-2xl font-bold;
+}
+</style>
