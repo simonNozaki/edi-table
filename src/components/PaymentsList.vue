167035df5bfdea7b71d1cbdce5ad6d86a7c6ffcd
snozaki <snozaki@netprotections.co.jp>
2023-05-02 21:04:34 +0900

    データセットの構造をリファクタリング

--- src/components/PaymentsList.vue
+++ src/components/PaymentsList.vue
@@ -8,7 +8,7 @@ import Select from './atoms/Select.vue';
 import InputLabel from './atoms/InputLabel.vue';
 import { Payment, usePayments } from '@/composables/usePayments';
 import { useTaxRates } from '@/composables/useTaxRates';
-const { getItemNamesById, payments } = usePayments()
+const { payments } = usePayments()
 const { taxRates } = useTaxRates()
 
 interface Props {
@@ -20,18 +20,26 @@ const props = defineProps<Props>()
 const openSideSheet = (payment: Payment) => {
   isOpenSideSheet.value = true
   selectedPayment.value = payment
+  console.log(selectedPayment)
 }
 
 const closeSideSheet = () => {
   isOpenSideSheet.value = false
 }
 
-const onSelectTaxRate = (id: number, e: { id: number, value: string }) => {
+const onSelectTaxRate = (id: number, paymentItemId: number, e: { id: number, value: string }) => {
   const payment = payments.value.filter((p) => p.id === id).at(0)
-  if (payment) {
-    payment.taxRate = e
-  }
+  if (!payment) return
+  const paymentItem = payment.paymentItems.filter((item) => item.id === paymentItemId).at(0)
+  if (!paymentItem) return
+  paymentItem.taxRate = e
 };
+
+const savePaymentItems = () => {
+  // TODO: 永続化など保存系のアクションをセットする、保存できたらスナックバー
+
+  closeSideSheet()
+}
 
 const isOpenSideSheet = ref(false)
 const selectedPayment = ref<Payment>()
@@ -41,40 +49,49 @@ const selectedPayment = ref<Payment>()
   <div>
     <SideSheet v-if="isOpenSideSheet">
       <div class="p-3">
-        <div class="">
-          <Button order="secondary" @click="closeSideSheet">閉じる</Button>
+        <div class="my-5">
+          <Button order="secondary" size="small" @click="closeSideSheet">閉じる</Button>
         </div>
-        <div class="m-5 grid grid-rows-3 gap-5">
+        <Card
+          v-for="(paymentItem, i) in selectedPayment?.paymentItems"
+          :key="paymentItem.id"
+          class="mb-5">
           <div>
+            {{ i + 1 }}つ目の商品 (id: {{ paymentItem.id }})
+          </div>
+          <div class="my-3">
             <InputLabel id="item-names">商品名</InputLabel>
-            <TextInput :model-value="getItemNamesById(selectedPayment?.id ?? 0)" id="item-names" />
+            <TextInput v-model="paymentItem.items" id="item-names" />
           </div>
-          <div>
+          <div class="my-3">
             <InputLabel id="amount">支払金額</InputLabel>
-            <NumberInput :model-value="selectedPayment?.amount ?? 0" id="amount" />
+            <NumberInput v-model="paymentItem.amount" id="amount" />
           </div>
-          <div>
+          <div class="my-3">
             <InputLabel id="tax-rate">適用税率</InputLabel>
-            <Select :options="taxRates" @select="onSelectTaxRate(selectedPayment?.id ?? 0, $event)" id="tax-rate" />
+            <Select :options="taxRates" @select="onSelectTaxRate(selectedPayment?.id ?? 0, paymentItem?.id ?? 0, $event)" id="tax-rate" />
           </div>
-        </div>
-        <Button>更新する</Button>
+        </Card>
+        <Button @click="savePaymentItems">更新する</Button>
       </div>
     </SideSheet>
     <div v-for="payment in props.values" :key="payment.id">
-      <Card clickable @click="openSideSheet(payment)">
+      <Card clickable @click="openSideSheet(payment)" class="my-2">
         <div class="grid card-grid">
           <div>
             <h3 class="card-title">
-              {{ getItemNamesById(payment.id) }}
+              ¥{{ payment.paymentItems.map((elm) => elm.amount).reduce((lhs, rhs) => lhs + rhs) }}
             </h3>
-            <span class="card-id">{{ payment.id }}</span>
+            <span class="card-id">id: {{ payment.id }}</span>
           </div>
-          <div class="ml-2">
-            <p>¥ {{ payment.amount }}</p>
-            <p>税 {{ payment.taxRate.value }}</p>
-            <p>支払日 {{ payment.payedAt }}</p>
-          </div>
+          <Card v-for="paymentItem in payment.paymentItems" :key="paymentItem.id">
+            <div class="ml-2">
+              <p>{{ paymentItem.items }}</p>
+              <p>¥ {{ paymentItem.amount }}</p>
+              <p>税 {{ paymentItem.taxRate.value }}</p>
+              <p>支払日 {{ paymentItem.payedAt }}</p>
+            </div>
+          </Card>
         </div>
       </Card>
     </div>
