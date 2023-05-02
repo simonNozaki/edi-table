18c071f1fb341ee16475cbb0178338b8ada94bf9
snozaki <snozaki@netprotections.co.jp>
2023-05-02 21:04:58 +0900

    リアクティブに反応できていなかった

--- src/components/atoms/NumberInput.vue
+++ src/components/atoms/NumberInput.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 interface Props {
   modelValue: number
 }
+interface Emits {
+  (e: 'update:modelValue', newValue: number): void
+}
+
 const props = withDefaults(defineProps<Props>(), {
   modelValue: 0
+})
+const emits = defineEmits<Emits>()
+
+const value = computed<number>({
+  get: () => props.modelValue,
+  set: (newValue: number) => {
+    emits('update:modelValue', newValue)
+  }
 })
 </script>
 
 <template>
-  <input type="number" v-model="props.modelValue">
+  <input type="number" v-model="value">
 </template>
 
 <style scoped>
--- src/components/atoms/TextInput.vue
+++ src/components/atoms/TextInput.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 interface Props {
   modelValue: string
+}
+interface Emits {
+  (e: 'update:modelValue', newValue: string): void
 }
 
 const props = withDefaults(defineProps<Props>(), {
   modelValue: ''
 })
+const emits = defineEmits<Emits>()
+
+const value = computed<string>({
+  get: () => props.modelValue,
+  set: (newValue: string) => {
+    emits('update:modelValue', newValue)
+  }
+})
 </script>
 
 <template>
-  <input type="text" v-model="props.modelValue">
+  <input type="text" v-model="value">
 </template>
 
 <style scoped>
